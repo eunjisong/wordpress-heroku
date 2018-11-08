1364e63422f983f0fb05b02926c8c09e8df21271
Eunji Song <eunji.l.song@gmail.com>
2018-11-08 14:57:42 -0500

    initial wordpress commit

--- wp-config.php
+++ wp-config.php
@@ -3,9 +3,9 @@
  * The base configurations of the WordPress.
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link http://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
  * installation. You don't have to use the web site, you can just copy this file
@@ -14,21 +14,18 @@
  * @package WordPress
  */
 
-// ** Heroku Postgres settings - from Heroku Environment ** //
-$db = parse_url($_ENV["DATABASE_URL"]);
-
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', trim($db["path"],"/"));
+define('DB_NAME', 'database_name_here');
 
 /** MySQL database username */
-define('DB_USER', $db["user"]);
+define('DB_USER', 'username_here');
 
 /** MySQL database password */
-define('DB_PASSWORD', $db["pass"]);
+define('DB_PASSWORD', 'password_here');
 
 /** MySQL hostname */
-define('DB_HOST', $db["host"]);
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -45,16 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',              getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY',       getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY',         getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY',             getenv('NONCE_KEY'));
-define('AUTH_SALT',             getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT',      getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT',        getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT',            getenv('NONCE_SALT'));
-define('AWS_ACCESS_KEY_ID',     getenv('AWS_ACCESS_KEY_ID'));
-define('AWS_SECRET_ACCESS_KEY', getenv('AWS_SECRET_ACCESS_KEY'));
+define('AUTH_KEY',         'j$b[$^ ZMZX,f1e^y]+02!$nDIAG_|dUkO 7JPa4A|tut1=*%[fqj1Id8v}`LYvL');
+define('SECURE_AUTH_KEY',  '3 -6pat.k!aM:HBSgJE<:h#W/%Wn6JmL8]m)CcK]d2^;Yt,kuG#]IlD_Mc%Wr|m5');
+define('LOGGED_IN_KEY',    'RuD[CqzkHv0$/txtJr8<aCJ]Il*p2:w6Nb &&3O*cCET^xH+@W$li[TN3QE/^)iW');
+define('NONCE_KEY',        'h:_V?%^q&!4F*6UvYjw_1a&!G(54ryc_cI_&R%!>Xt*hC7&1>zMSuk,/+n}zx3>_');
+define('AUTH_SALT',        '!7t~`)JN*+{s5(b|F]Z,:tuJUG^L&@2zA?8NJ/o$Sb|C:X;-BG8LgK%%5{RCm@67');
+define('SECURE_AUTH_SALT', '$S7wysL|Pjx_T5H!z=I_|8UZ#vzW;0GzV+ac?CpC^DbV4kG1`Z=z_O>X#:uLTwU<');
+define('LOGGED_IN_SALT',   'zxU)hQ}@:v_+;{e]KgW&|t`P(,T:VMm3QU0e.!UbMUjgA_+@~zH;d+$q-#42:0=1');
+define('NONCE_SALT',       '6q]i|d+SrsXDg)-uf:G2)(%n6rfnMMnG`CypYsU>yJG}Be,{er&/{=y/*K+B+.Zd');
 
 /**#@-*/
 
